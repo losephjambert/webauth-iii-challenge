0353b5e5aacd399bb130672d4e9774d2d8a77edd
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-13 21:57:20 -0800

    creates base route for auth and users via combined router

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 
+const apiRouter = require('./api-router.js')
+
 const server = express()
+
+server.use('/api', apiRouter)
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: `api is up` })
